b8dd06927f7bb8c0ae888b7c3322eb92e8731423
jhoni-costa <jhoni_costa@outlook.com>
2019-05-07 21:39:58 -0300

    Pagina about

---
+++ about.php
@@ -0,0 +1,47 @@
+<!DOCTYPE HTML>
+<html lang="pt">
+    <head>
+    
+        <meta charset="UTF-8">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    </head>
+
+    <body>
+        <div class="container">
+
+        <?php include 'view/toolbar.php' ?>
+
+            <div class="alert alert-info" role="alert">
+                Site em Desenvolvimento! 
+            </div>
+
+            <div class="card text-center">
+                <div class="center card-header">
+                    <img src="img/profile.jpg" class="img-fluid img-thumbnail" width="140" height="140" alt="profile">
+                </div>
+            
+                <div class="card-body">
+                    <h5 class="card-title">About Jhoni Costa</h5>
+                    <p class="card-text">Jhoni Regis Souza da Costa, criador do site Desenvolvimento Expert, 
+                    Brasileiro natural de Araucária, noivo da Jéssica, Cristão, Tocador de violão e jogador de FIFA nas horas vagas (que estão escassas).</p>
+                    <p>Cursei Engenharia de Produção até o último periodo descobri que não era o que queria, desde então me intruduzir no mundo de desenvolvimento, 
+                    ja cursei Analise e Desenvolvimento de Sistemas e depois de muitas idas e vindas me achei no EAD e agora vou até o fim.</p>
+                    <p>Entre muitos altos e baixos da vida, acabei trabalhando de professor de informatica e atualmente sou cobrador de onibus, sei que tenho muito potencial e estou em busca de progredir.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+                <div class="card-footer text-muted">
+                    07-maio-2019
+                </div>
+            </div>
+
+        </div>
+    </body>
+
+</html>
---
+++ view/toolbar.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="index.php">
+                    <img src="img/icone.png" width="40" height="40" alt="logo site">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="index.php">Inicio <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.php">Sobre</a>
+                    </li>
+                    </ul>
+                    <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Busca">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Busca</button>
+                    </form>
+                </div>
+            </nav>
